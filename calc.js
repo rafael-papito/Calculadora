d71288b1784f8e484ca7b43971df598e6fbbcace
Rafael Porto <rafaelgportof@gmail.com>
2021-07-27 17:37:27 -0300

    function igual
    Claramente é onde nos teremos o resultado de todas as nossas operações.

--- calc.js
+++ calc.js
@@ -1,3 +1,9 @@
 function insert(num){
     document.form.textview.value = document.form.textview.value + num
 }
+function igual(){
+    exp = document.form.textview.value
+    if(exp){
+        document.form.textview.value = eval(exp)
+    }
+ }
