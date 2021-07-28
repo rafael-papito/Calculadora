cb45af807247004a05f9eb08329d26f86493a590
Rafael Porto <rafaelgportof@gmail.com>
2021-07-28 08:50:28 -0300

    function back
    Essa function é onde vamos poder excluir o ultimo caractere ou número chamado.

--- calc.js
+++ calc.js
@@ -10,3 +10,7 @@ function igual(){
 function c(){
     document.form.textview.value = ""
 }
+function back(){
+    var exp = document.form.textview.value
+    document.form.textview.value = exp.substring(0, exp.length-1)
+}
